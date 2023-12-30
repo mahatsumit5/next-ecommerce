44d58e245d0705e3968c3b6cc8515b60a6c95f58
Sumit Mahat <mahatsumit5@gmail.com>
2023-12-30 23:30:12 +1100

    PNG IMAGE

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,7 +4,7 @@ import { Button } from "./ui/button";
 
 const Hero = () => {
   return (
-    <div className="  bg-dotted-pattern  wrapper flex justify-between items-center h-full ">
+    <div className="  wrapper flex flex-row  justify-between items-center h-full  ">
       <div className="flex flex-col  gap-7">
         <p className="text-xl text-muted-foreground">
           Get your winter Jacket Now!!
