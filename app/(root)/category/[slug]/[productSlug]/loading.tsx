19df42618e6f673af7fd023ae261fa4730ada65c
Sumit Mahat <mahatsumit5@gmail.com>
2024-01-11 20:44:51 +1100

    loading features

--- app/(root)/category/[slug]/[productSlug]/loading.tsx
+++ app/(root)/category/[slug]/[productSlug]/loading.tsx
@@ -3,12 +3,12 @@ import React from "react";
 
 function loading() {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-      <div className="flex flex-col  gap-4 ">
-        <Skeleton className="w-full h-[500px" />
-        <Skeleton className="w-full h-[200px]" />
+    <div className="grid grid-cols-1 md:grid-cols-2  gap-4 w-full">
+      <div className="w-full  flex-col  gap-5">
+        <Skeleton className="w-full h-[60evh]" />
+        <Skeleton className="w-full h-[80evh]" />
       </div>
-      <div className="  flex flex-col gap-4 ">
+      <div className="  w-full flex  flex-col gap-4 ">
         <Skeleton className="w-full h-[500px]" />
 
         <Skeleton className="w-full h-[200px]" />
