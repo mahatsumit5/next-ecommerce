6d2ba4c3cc107a923bad90dede9bee6297678297
Sumit Mahat <mahatsumit5@gmail.com>
2024-01-11 21:22:10 +1100

    vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/ui/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import {
   dark,
   neobrutalism,
@@ -31,6 +32,7 @@ export default function RootLayout({
     >
       <html>
         <body className={`${raleway.className}  `}>
+          <SpeedInsights />
           <ThemeProvider
             attribute="class"
             defaultTheme="light"
