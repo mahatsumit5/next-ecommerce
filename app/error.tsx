bfb9547c4d1921af3ca4ce34f2b175ac3395aac1
Sumit Mahat <mahatsumit5@gmail.com>
2024-03-15 02:53:52 +1100

    error page

---
+++ app/error.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function error() {
+  return (
+    <div>
+      <h2>Something went wrong!</h2>
+      <button>Try again</button>{" "}
+    </div>
+  );
+}
+
+export default error;
